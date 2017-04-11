0c98834f4249a42e028e5bb231c15331d9e2ee3d
François Garillot <francois@garillot.net>
2017-04-11 16:15:30 -0400

    Update to the latest release

--- dl4j-spark-benchmark/src/main/java/org/deeplearning4j/train/config/BaseSparkTest.java
+++ dl4j-spark-benchmark/src/main/java/org/deeplearning4j/train/config/BaseSparkTest.java
@@ -1,13 +1,13 @@
 package org.deeplearning4j.train.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.nd4j.shade.jackson.annotation.JsonAutoDetect;
+import org.nd4j.shade.jackson.annotation.JsonIgnoreProperties;
+import org.nd4j.shade.jackson.annotation.PropertyAccessor;
+import org.nd4j.shade.jackson.core.JsonFactory;
+import org.nd4j.shade.jackson.databind.DeserializationFeature;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
+import org.nd4j.shade.jackson.databind.SerializationFeature;
+import org.nd4j.shade.jackson.dataformat.yaml.YAMLFactory;
 import lombok.Data;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.spark.api.Repartition;
